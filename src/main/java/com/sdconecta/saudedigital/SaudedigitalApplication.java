d18ae13ceca0826ad11d32ac263c59e3e9a2dbdd
Lucas Fernando <lucasfernandolf38@gmail.com>
2022-06-28 17:15:29 -0300

    Initial commit

---
+++ src/main/java/com/sdconecta/saudedigital/SaudedigitalApplication.java
@@ -0,0 +1,13 @@
+package com.sdconecta.saudedigital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaudedigitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaudedigitalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sdconecta/saudedigital/SaudedigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sdconecta.saudedigital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaudedigitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
