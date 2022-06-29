f5bc94fa7a502ebaae33fc963a00d0b265b5c8bd
Lucas Fernando <lucasfernandolf38@gmail.com>
2022-06-29 17:20:23 -0300

    UserController

---
+++ src/main/java/com/sdconecta/saudedigital/controllers/UserController.java
@@ -0,0 +1,54 @@
+package com.sdconecta.saudedigital.controllers;
+
+import com.sdconecta.saudedigital.models.User;
+import com.sdconecta.saudedigital.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/users")
+public class UserController {
+
+    @Autowired
+    private UserService service;
+
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User obj){
+        User response = service.create(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}").buildAndExpand(response.getId()).toUri();
+        return ResponseEntity.created(uri).body(response);
+    }
+
+    @PutMapping(value = "/{id}")
+    public  ResponseEntity<User> update(@PathVariable Integer id, @RequestBody User obj){
+        service.update(id, obj);
+        return ResponseEntity.ok().body(obj);
+
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> findAll(){
+        List<User> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Integer id){
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Integer id){
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+
+    }
+}
