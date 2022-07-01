252c34cf9498ee7ac2c28e91c8917fbd52c53bb0
Lucas Fernando <lucasfernandolf38@gmail.com>
2022-07-01 17:23:16 -0300

    Fix: Endpoint to add crm on the user

--- src/main/java/com/sdconecta/saudedigital/controllers/CrmController.java
+++ src/main/java/com/sdconecta/saudedigital/controllers/CrmController.java
@@ -1,7 +1,9 @@
 package com.sdconecta.saudedigital.controllers;
 
 import com.sdconecta.saudedigital.models.Crm;
+import com.sdconecta.saudedigital.models.User;
 import com.sdconecta.saudedigital.services.CrmService;
+import com.sdconecta.saudedigital.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,43 +13,49 @@ import java.net.URI;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/crms")
+@RequestMapping("/api/v1")
 public class CrmController {
 
     @Autowired
-    private CrmService service;
+    private CrmService crmService;
 
-    @PostMapping
-    public ResponseEntity<Crm> insert(@RequestBody Crm obj){
-        Crm response = service.create(obj);
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/users/{userId}/crms")
+    public ResponseEntity<Crm> insert(@PathVariable Integer userId, @RequestBody Crm obj){
+        User user = userService.findById(userId);
+        obj.setUser(user);
+        Crm response = crmService.create(obj);
+
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
                 .path("/{id}").buildAndExpand(response.getId()).toUri();
         return ResponseEntity.created(uri).body(response);
     }
 
-    @PutMapping(value = "/{id}")
+    @PutMapping(value = "/crms/{id}")
     public  ResponseEntity<Crm> update(@PathVariable Integer id, @RequestBody Crm obj){
-        service.update(id, obj);
+        crmService.update(id, obj);
         return ResponseEntity.ok().body(obj);
 
     }
 
-    @GetMapping
+    @GetMapping(value = "/crms")
     public ResponseEntity<List<Crm>> findAll(){
-        List<Crm> list = service.findAll();
+        List<Crm> list = crmService.findAll();
         return ResponseEntity.ok().body(list);
     }
 
-    @DeleteMapping(value = "/{id}")
+    @DeleteMapping(value = "/crms/{id}")
     public ResponseEntity<Void> delete(@PathVariable Integer id){
-        service.delete(id);
+        crmService.delete(id);
         return ResponseEntity.noContent().build();
 
     }
 
-    @GetMapping(value = "/{id}")
+    @GetMapping(value = "crms/{id}")
     public ResponseEntity<Crm> findById(@PathVariable Integer id){
-        Crm obj = service.findById(id);
+        Crm obj = crmService.findById(id);
         return ResponseEntity.ok().body(obj);
 
     }
